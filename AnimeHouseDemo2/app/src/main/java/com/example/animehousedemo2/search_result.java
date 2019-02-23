bcb11b2a69ca7f33066995e36e8f9c211ca29585
mohamed12samy <ma2519299@gmail.com>
2019-02-23 16:11:07 +0200

    Search result code

--- AnimeHouseDemo2/app/src/main/java/com/example/animehousedemo2/search_result.java
+++ AnimeHouseDemo2/app/src/main/java/com/example/animehousedemo2/search_result.java
@@ -41,8 +41,62 @@ public class search_result extends AppCompatActivity {
     }
 
 
+    
+	
     public void getData(String Url) {
 
+        String url = Url;
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        //anName.setText("Response: " + response.toString());
+
+                        //title -- score - image_url - type - start_date - url - synopsis
+                        try {
+                            JSONArray jr = response.getJSONArray("results");
+
+                            for (int i = 0; i < jr.length(); i++) {
+                                search = new search_item();
+                                JSONObject jsonObject = jr.getJSONObject(i);
+
+                                search.setTitle(jsonObject.getString("title"));
+                                search.setScore(jsonObject.getInt("score"));
+                                search.setUrl(jsonObject.getString("url"));
+                                search.setDate(jsonObject.getString("start_date"));
+                                search.setImage_url(jsonObject.getString("image_url"));
+                                search.setType(jsonObject.getString("type"));
+                                search.setSynopsis(jsonObject.getString("synopsis"));
+                                searchList.add(search);
+                                System.out.println(searchList.get(i).getTitle());
+                            }
+
+                            // Adds the data string to the TextView "results"
+                            listView.setAdapter(new search_Adapter(getApplicationContext(),0,searchList));
+
+                        }
+                        // Try and catch are included to handle any errors due to JSON
+                        catch (JSONException e) {
+                            // If an error occurs, this prints the error to the log
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+                        Toast.makeText(search_result.this, "error", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        // Access the RequestQueue through your singleton class.
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
     }
+
+    
 
 }
